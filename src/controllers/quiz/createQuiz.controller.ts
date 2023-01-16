bfd89e0e95058400e535e4f007eb5371f701ee82
Lucas <lucas-leite-menezes@hotmail.com>
2023-01-16 12:17:51 -0300

    Revert "feat: add quiz routes"
    This reverts commit 80cc65669f3e826f41f071720319ace8bbfeb7b6.

--- src/controllers/quiz/createQuiz.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-import { createQuizService } from "../../services/quiz/createQuiz.service";
-
-export const createQuizController = async (req: Request, res: Response) => {
-  const quizData = await createQuizService(req.body);
-  return res.status(201).json(quizData);
-};
--- src/interfaces/quizzes/index.ts
+++ src/interfaces/quizzes/index.ts
@@ -5,16 +5,6 @@ export interface IQuizzes {
   questions: IQuestions;
 }
 
-export interface IQuizzesUpdate {
-  name?: string;
-  questions?: IQuestions;
-}
-
-export interface IQuizzesResponse {
-  name: string;
-  questions?: IQuestions;
-}
-
 export interface IQuizzesQuestions {
   quizzesId: string;
   questionsId: string;
